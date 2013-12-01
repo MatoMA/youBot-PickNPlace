91cb71bb75e257a773885b0274c2a2ce7fcb0509
Mato <mato.q0o0p@gmail.com>
2013-12-01 20:39:39 +0100

    Project init

---
+++ Ros_Workspace/playGround/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/mato/workspace/youBot-PickNPlace/Ros_Workspace/playGround/src
+# Build directory: /home/mato/workspace/youBot-PickNPlace/Ros_Workspace/playGround/build
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(beginner_tutorials)
---
+++ Ros_Workspace/playGround/build/beginner_tutorials/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mato/workspace/youBot-PickNPlace/Ros_Workspace/playGround/src/beginner_tutorials
+# Build directory: /home/mato/workspace/youBot-PickNPlace/Ros_Workspace/playGround/build/beginner_tutorials
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
