629c769abf3e380abd9a5c99891b938d52953d47
Mato <mato.q0o0p@gmail.com>
2013-12-01 20:47:31 +0100

    add .gitignore

--- Ros_Workspace/playGround/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mato/workspace/youBot-PickNPlace/Ros_Workspace/playGround/src
-# Build directory: /home/mato/workspace/youBot-PickNPlace/Ros_Workspace/playGround/build
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
-SUBDIRS(beginner_tutorials)
--- Ros_Workspace/playGround/build/beginner_tutorials/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mato/workspace/youBot-PickNPlace/Ros_Workspace/playGround/src/beginner_tutorials
-# Build directory: /home/mato/workspace/youBot-PickNPlace/Ros_Workspace/playGround/build/beginner_tutorials
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
